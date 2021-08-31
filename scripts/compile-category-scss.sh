0a498b7519a760df48ad19b41e7e4ec8ed3d5465
Oscar Mattsson <oscar.mattsson@se.nordddb.com>
2021-08-31 08:27:52 +0200

    Adds scripts for compiling scss -> css

---
+++ scripts/compile-category-scss.sh
@@ -0,0 +1,15 @@
+colours=/colours/colours.scss
+dialogues=/dialogues/dialogues.scss
+icons=/icons/icons.scss
+image=/image/image.scss
+indicators=/indicators/indicators.scss
+input=/input/input.scss
+logos=/logos/logos.scss
+modules=/modules/modules.scss
+navigation=/navigation/navigation.scss
+selection=/selection/selection.scss
+structure=/structure/structure.scss
+tables=/tables-and-lists/tables-and-lists.scss
+typography=/typography/typography.scss
+
+yarn sass --no-source-map  src/components/style/$colours:src/style/example-css/colours.css src/components/style/$dialogues:src/style/example-css/dialogues.css src/components/style/$icons:src/style/example-css/icons.css src/components/style/$image:src/style/example-css/image.css src/components/style/$indicators:src/style/example-css/indicators.css src/components/style/$input:src/style/example-css/input.css src/components/style/$logos:src/style/example-css/logos.css src/components/style/$modules:src/style/example-css/modules.css src/components/style/$navigation:src/style/example-css/navigation.css src/components/style/$selection:src/style/example-css/selection.css src/components/style/$structure:src/style/example-css/structure.css src/components/style/$tables:src/style/example-css/tables-and-lists.css src/components/style/$typography:src/style/example-css/typography.css
---
+++ scripts/compile-component-scss.sh
@@ -0,0 +1,75 @@
+colours=colours/colours.scss
+
+# DIALOGUES
+gdpr=dialogues/gdpr-consent.scss
+messageBanner=dialogues/message-banners.scss
+notification=dialogues/notification.scss
+tooltip=dialogues/tooltip.scss
+
+# ICONS
+icons=icons/icons.scss
+
+# IMAGE
+imageWithCaption=image/image-with-caption.scss
+
+# INDICATORS
+badge=indicators/badge.scss
+pagination=indicators/pagination.scss
+preloader=indicators/preloader.scss
+progressIndicator=indicators/progress-indicator.scss
+progress=indicators/progress.scss
+spinner=indicators/spinner.scss
+
+
+# INPUT
+input=input/input.scss
+
+# LOGOS
+topBar=logos/top-bar.scss
+
+# MODULES
+article=modules/article/article.scss
+carousel=modules/carousel.scss
+footer=modules/footer.scss
+form=modules/form.scss
+imageBlock=modules/image-block.scss
+linkedList=modules/linked-list.scss
+media=modules/media.scss
+offsetImage=modules/offset-image.scss
+quotes=modules/quotes.scss
+subscription=modules/subscription.scss
+upload=modules/upload.scss
+
+# NAVIGATION 
+header=navigation/header.scss
+tabBar=navigation/tab-bar.scss
+textLink=navigation/text-link.scss
+
+# SELECTION
+buttons=selection/buttons.scss
+calendar=selection/calendar.scss
+dropdowns=selection/dropdowns.scss
+option=selection/option.scss
+
+# STRUCTURE
+grid=structure/grid.scss
+responsive=structure/responsive.scss
+sizing=structure/sizing.scss
+spacing=structure/spacing.scss
+
+# TABLES AND LISTS
+linkList=tables-and-lists/link-list.scss
+numberedList=tables-and-lists/numbered-list.scss
+standardList=tables-and-lists/standard-list.scss
+table=tables-and-lists/table.scss
+
+# TYPOGRAPHY
+bodyCopy=typography/body-copy.scss
+emphasis=typography/emphasis.scss
+errors=typography/errors.scss
+headers=typography/headers.scss
+intro=typography/intro.scss
+otherTextStyles=typography/other-text-styles.scss
+
+
+yarn sass --no-source-map  src/components/style/$colours:src/style/component-css/colours.css src/components/style/$gdpr:src/style/component-css/gdpr.css src/components/style/$messageBanner:src/style/component-css/message-banners.css src/components/style/$notification:src/style/component-css/notification.css src/components/style/$tooltip:src/style/component-css/tooltip.css src/components/style/$icons:src/style/component-css/icons.css src/components/style/$imageWithCaption:src/style/component-css/image-with-caption.css src/components/style/$badge:src/style/component-css/badge.css src/components/style/$pagination:src/style/component-css/pagination.css  src/components/style/$preloader:src/style/component-css/preloader.css  src/components/style/$progressIndicator:src/style/component-css/progress-indicator.css src/components/style/$progress:src/style/component-css/progress.css  src/components/style/$spinner:src/style/component-css/spinner.css src/components/style/$input:src/style/component-css/input.css  src/components/style/$topBar:src/style/component-css/top-bar.css src/components/style/$article:src/style/component-css/article.css src/components/style/$carousel:src/style/component-css/carousel.css src/components/style/$footer:src/style/component-css/footer.css src/components/style/$form:src/style/component-css/form.css src/components/style/$imageBlock:src/style/component-css/image-block.css src/components/style/$linkedList:src/style/component-css/linked-list.css src/components/style/$media:src/style/component-css/media.css src/components/style/$offsetImage:src/style/component-css/offset-image.css src/components/style/$quotes:src/style/component-css/quotes.css src/components/style/$subscription:src/style/component-css/subscription.css src/components/style/$upload:src/style/component-css/upload.css src/components/style/$header:src/style/component-css/header.css src/components/style/$tabBar:src/style/component-css/tab-bar.css src/components/style/$textLink:src/style/component-css/text-link.css src/components/style/$buttons:src/style/component-css/buttons.css src/components/style/$calendar:src/style/component-css/calendar.css src/components/style/$dropdowns:src/style/component-css/dropdowns.css src/components/style/$option:src/style/component-css/option.css src/components/style/$grid:src/style/component-css/grid.css src/components/style/$responsive:src/style/component-css/responsive.css src/components/style/$sizing:src/style/component-css/sizing.css src/components/style/$spacing:src/style/component-css/spacing.css src/components/style/$linkList:src/style/component-css/link-list.css src/components/style/$numberedList:src/style/component-css/numbered-list.css src/components/style/$standardList:src/style/component-css/standard-list.css src/components/style/$table:src/style/component-css/table.css src/components/style/$bodyCopy:src/style/component-css/body-copy.css src/components/style/$emphasis:src/style/component-css/emphasis.css src/components/style/$errors:src/style/component-css/errors.css src/components/style/$headers:src/style/component-css/headers.css src/components/style/$intro:src/style/component-css/intro.css src/components/style/$otherTextStyles:src/style/component-css/other-text-styles.css 
